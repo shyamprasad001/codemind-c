c0e3ead3a09540f3b6e6818f56097ab88090f423
shyamprasad001 <147488280+shyamprasad001@users.noreply.github.com>
2023-10-15 15:59:22 +0530

    #include<stdio.h> int main() {     int X,Y,a;     scanf("%d%d",&X,&Y);     a=X-Y;     if(X>Y)     {         printf("%d",a);     }     else if(X<Y)     {         printf("0");     }     else     {         printf("0");     }
    }

---
+++ Chairs_Requirement.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main()
+{
+    int X,Y,a;
+    scanf("%d%d",&X,&Y);
+    a=X-Y;
+    if(X>Y)
+    {
+        printf("%d",a);
+    }
+    else if(X<Y)
+    {
+        printf("0");
+    }
+    else 
+    {
+        printf("0");
+    }
+    
+}
