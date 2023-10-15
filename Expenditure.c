5368e91c856ed048d80e26a2b9d29c9e54d7f977
shyamprasad001 <147488280+shyamprasad001@users.noreply.github.com>
2023-10-15 10:45:03 +0530

    #include<stdio.h> int main() {     int X,Y,A;     scanf("%d%d",&X,&Y);     A=Y*30;     if(X>A)     {         printf("YES");     }     else if(X<A)     {         printf("NO");     }     else     {         printf("YES");     } }

---
+++ Expenditure.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main()
+{
+    int X,Y,A;
+    scanf("%d%d",&X,&Y);
+    A=Y*30;
+    if(X>A)
+    {
+        printf("YES");
+    }
+    else if(X<A)
+    {
+        printf("NO");
+    }
+    else
+    {
+        printf("YES");
+    }
+}
