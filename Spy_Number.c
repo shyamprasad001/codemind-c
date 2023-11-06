da10083cf37dd51d909676ad3cca2471a71be7a0
shyamprasad001 <147488280+shyamprasad001@users.noreply.github.com>
2023-11-06 15:49:26 +0530

    #include<stdio.h> int main() { 	int n,r,sum=0,product=1; 	scanf("%d",&n); 	while(n!=0) 	{ 	r=n%10; 	sum=sum+r; 	n=n/10; 	product=product*r; 	} 	if(sum==product) 	{ 		printf("Spy Number"); 	} 	else 	{ 		printf("Not Spy Number"); 	}
    }

--- Spy_Number.c
+++ Spy_Number.c
@@ -1,29 +1,24 @@
+
 #include<stdio.h>
 int main()
 {
-    int n,r,sum=0,mul=1,x,y,v;
-    scanf("%d",&n);
-    v=n;
-    while(n!=0)
-    {
-        r=n%10;
-        sum=sum+r;
-        n=n/10;
-    }
-    x=sum;
-    while(v!=0)
-    {
-        r=v%10;
-        mul=mul*r;
-        v=v/10;
-    }
-    y=mul;
-    if(x==y)
-    {
-        printf("Spy Number");
-    }
-    else
-    {
-        printf("Not Spy Number");
-    }
+	int n,r,sum=0,product=1;
+	scanf("%d",&n);
+	while(n!=0)
+	{
+	r=n%10;
+	sum=sum+r;
+	n=n/10;
+	product=product*r;
+	}
+	if(sum==product)
+	{
+		printf("Spy Number");
+	}
+	else
+	{
+		printf("Not Spy Number");
+	}
+    
+	
 }
