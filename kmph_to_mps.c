e59262fade9bff8953a6e2bd6e1834eb6e4152c8
Shyam Mantri <147488280+shyamprasad001@users.noreply.github.com>
2023-12-09 18:41:25 +0530

    #include<stdio.h> int main() {     int x;     float y;     scanf("%d",&x);     y=x/3.6;     printf("%.2f",y);
    }

--- kmph_to_mps.c
+++ kmph_to_mps.c
@@ -1,9 +1,10 @@
 #include<stdio.h>
 int main()
 {
-    int n;
-    float m;
-    scanf("%d",&n);
-    m=n*(0.2777777777777);
-    printf("%.2f",m);
+    int x;
+    float y;
+    scanf("%d",&x);
+    y=x/3.6;
+    printf("%.2f",y);
+    
 }
