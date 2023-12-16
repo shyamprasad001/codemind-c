4c67c451d7ef58c6bc6aaa3802275f236027d0d3
Shyam Mantri <147488280+shyamprasad001@users.noreply.github.com>
2023-12-16 14:35:40 +0530

    #include<stdio.h> int main() {     int x,y,z,l;     scanf("%d%d%d%d",&x,&y,&z,&l);     if(x>=10 && y>=10 && z>=10 && l>=10)     printf("YES");     else     printf("NO"); }

--- Target.c
+++ Target.c
@@ -1,15 +1,10 @@
 #include<stdio.h>
 int main()
 {
-    int p1,p2,p3,p4,tot;
-    scanf("%d%d%d%d",&p1,&p2,&p3,&p4);
-    tot=p1+p2+p3+p4;
-    if(p1>9 && p2>9 && p3>9 && p4>9)
-    {
-        printf("YES");
-    }
+    int x,y,z,l;
+    scanf("%d%d%d%d",&x,&y,&z,&l);
+    if(x>=10 && y>=10 && z>=10 && l>=10)
+    printf("YES");
     else
-    {
-        printf("NO");
-    }
+    printf("NO");
 }
