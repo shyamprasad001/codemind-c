af7affab5c3a64275127c8809e3dbdd792a7c16f
Shyam Mantri <147488280+shyamprasad001@users.noreply.github.com>
2023-12-18 22:12:10 +0530

    #include<stdio.h> int main() {     int a,i;     scanf("%d",&a);     for(i=1;i<=a;i++)     {         printf("%d ",i);     } }

--- 1_to_n.c
+++ 1_to_n.c
@@ -1,11 +1,10 @@
 #include<stdio.h>
 int main()
 {
-    int i,n;
-    scanf("%d",&n);
-    for(i=1;i<=n;i++)
+    int a,i;
+    scanf("%d",&a);
+    for(i=1;i<=a;i++)
     {
         printf("%d ",i);
     }
-    
 }
