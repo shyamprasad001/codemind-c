d522f0a010c3345331b3666313422109ff86a446
Shyam Mantri <147488280+shyamprasad001@users.noreply.github.com>
2023-12-29 17:32:12 +0530

    #include<stdio.h> int main() {     float X,Y;     scanf("%f%f",&X,&Y);     float Per;     Per=(Y-X)/X*100;     printf("%.2f",Per); }

--- Profit_Percentage.c
+++ Profit_Percentage.c
@@ -1,8 +1,9 @@
 #include<stdio.h>
 int main()
 {
-    float x,y,z;
-    scanf("%f%f",&x,&y);
-    z=(y-x)/x*100;
-    printf("%.2f",z);
+    float X,Y;
+    scanf("%f%f",&X,&Y);
+    float Per;
+    Per=(Y-X)/X*100;
+    printf("%.2f",Per);
 }
