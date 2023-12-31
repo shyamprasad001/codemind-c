67d8d23e2c411c3afcfe0e38c61cf7e96e58d38d
Shyam Mantri <147488280+shyamprasad001@users.noreply.github.com>
2023-12-31 22:46:08 +0530

    #include<stdio.h> int main() {     int k,v[2004],l,r;     scanf("%d",&l);     for(k=0;k<l;k++)     {         scanf("%d",&v[k]);     }     for(k=l-1;k>=0;k--)     {         if(v[k]%2==0)         {             r=k;             break;         }     }     printf("%d",r); }

---
+++ Last_even_element_index.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main()
+{
+    int k,v[2004],l,r;
+    scanf("%d",&l);
+    for(k=0;k<l;k++)
+    {
+        scanf("%d",&v[k]);
+    }
+    for(k=l-1;k>=0;k--)
+    {
+        if(v[k]%2==0)
+        {
+            r=k;
+            break;
+        }
+    }
+    printf("%d",r);
+}
