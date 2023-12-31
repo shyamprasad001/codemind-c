c19a3ebf30395c31fb33f914a0370d2644625ac2
Shyam Mantri <147488280+shyamprasad001@users.noreply.github.com>
2023-12-31 23:01:55 +0530

    #include<stdio.h> int main() {     int r,v[2004],k,l=0,m=0;     scanf("%d",&r);     for(k=0;k<r;k++)     {         scanf("%d",&v[k]);     }     for(k=0;k<r;k++)     {         if(v[k]%2!=0)         {             l+=v[k];         }     }     printf("%d",l); }

--- Odd_Sum.c
+++ Odd_Sum.c
@@ -1,16 +1,18 @@
 #include<stdio.h>
 int main()
 {
-    int j,n,sum=0;
-    int arr[n];
-    scanf("%d",&n);
-    for(j=0;j<=n;j++){
-        scanf("%d",&arr[j]);
+    int r,v[2004],k,l=0,m=0;
+    scanf("%d",&r);
+    for(k=0;k<r;k++)
+    {
+        scanf("%d",&v[k]);
     }
-    for(j=0;j<=n;j++)
-    if(arr[j]%2==1){
-        sum=sum+arr[j];
+    for(k=0;k<r;k++)
+    {
+        if(v[k]%2!=0)
+        {
+            l+=v[k];
+        }
     }
-    printf("%d",sum);
-    
+    printf("%d",l);
 }
