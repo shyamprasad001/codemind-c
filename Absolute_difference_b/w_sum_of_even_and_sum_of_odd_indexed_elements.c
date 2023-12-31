4ebb01c28701fc09f5a3825ff3929ffff9c5bfc0
Shyam Mantri <147488280+shyamprasad001@users.noreply.github.com>
2023-12-31 22:49:18 +0530

    #include<stdio.h> int main() {     int r,v[2004],k,l=0,m=0;     scanf("%d",&r);     for(k=0;k<r;k++)     {         scanf("%d",&v[k]);     }     for(k=0;k<r;k++)     {         if(k%2==0)         {             l+=v[k];         }         else         {             m+=v[k];         }     }     printf("%d",l-m); }

---
+++ Absolute_difference_b/w_sum_of_even_and_sum_of_odd_indexed_elements.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int main()
+{
+    int r,v[2004],k,l=0,m=0;
+    scanf("%d",&r);
+    for(k=0;k<r;k++)
+    {
+        scanf("%d",&v[k]);
+    }
+    for(k=0;k<r;k++)
+    {
+        if(k%2==0)
+        {
+            l+=v[k];
+        }
+        else
+        {
+            m+=v[k];
+        }
+    }
+    printf("%d",l-m);
+}
