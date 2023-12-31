5e172ddd8f6b36119405ae1a0882b2efdd0baeed
Shyam Mantri <147488280+shyamprasad001@users.noreply.github.com>
2023-12-31 22:38:01 +0530

    #include<stdio.h> int main() {     int r,v[2004],k,a=0,l=0;     scanf("%d",&r);     for(k=0;k<r;k++)     {         scanf("%d",&v[k]);     }     for(k=0;k<r;k++)     {         a+=v[k];     }     a=a/r;     for(k=0;k<r;k++)     {         if(v[k]>=a)         {             l++;         }     }     printf("%d",l); }

---
+++ Elements_greater_than_average.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+int main()
+{
+    int r,v[2004],k,a=0,l=0;
+    scanf("%d",&r);
+    for(k=0;k<r;k++)
+    {
+        scanf("%d",&v[k]);
+    }
+    for(k=0;k<r;k++)
+    {
+        a+=v[k];
+    }
+    a=a/r;
+    for(k=0;k<r;k++)
+    {
+        if(v[k]>=a)
+        {
+            l++;
+        }
+    }
+    printf("%d",l);
+}
