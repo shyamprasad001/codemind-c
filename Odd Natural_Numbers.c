e023aaf02905a2ef0efb9a37ee501668f6f569fe
Shyam Mantri <147488280+shyamprasad001@users.noreply.github.com>
2024-01-01 18:59:27 +0530

    #include<stdio.h> int main() {     int n,i;     scanf("%d",&n);     for(i=1;i<=n;i++){         if(i%2!=0)         printf("%d ",i);     }
    }

--- Odd Natural_Numbers.c
+++ Odd Natural_Numbers.c
@@ -3,14 +3,9 @@ int main()
 {
     int n,i;
     scanf("%d",&n);
-    if(n!=0)
-    {
-        for(i=1;i<=n;i++)
-        {
-            if(i%2!=0)
-            {
-                printf("%d ",i);
-            }
-        }
+    for(i=1;i<=n;i++){
+        if(i%2!=0)
+        printf("%d ",i);
     }
+    
 }
