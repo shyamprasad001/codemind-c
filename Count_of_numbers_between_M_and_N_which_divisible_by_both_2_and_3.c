c6f957f1f589c34d19253b39f66ff0801ee9c08c
Shyam Mantri <147488280+shyamprasad001@users.noreply.github.com>
2024-01-01 21:14:43 +0530

    #include<stdio.h> int main() {     int n,m,i,cou=0;     scanf("%d%d",&n,&m);     for(i=n;i<=m;i++)     {          if((i%2==0) && (i%3==0))
            cou=cou+1;
        }
        printf("%d",cou);
            
        
    }

--- Count_of_numbers_between_M_and_N_which_divisible_by_both_2_and_3.c
+++ Count_of_numbers_between_M_and_N_which_divisible_by_both_2_and_3.c
@@ -1,15 +1,15 @@
 #include<stdio.h>
 int main()
 {
-    int n,m,i,count=0;
+    int n,m,i,cou=0;
     scanf("%d%d",&n,&m);
-    
     for(i=n;i<=m;i++)
     {
-        if(i%2==0 && i%3==0)
-        {
-            count++;
-        }
+         if((i%2==0) && (i%3==0))
+    
+        cou=cou+1;
     }
-    printf("%d",count);
+    printf("%d",cou);
+        
+    
 }
