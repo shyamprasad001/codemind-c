ada005a210554de1a5e137cd297d9562a28a3492
Shyam Mantri <147488280+shyamprasad001@users.noreply.github.com>
2024-01-01 18:55:30 +0530

    #include<stdio.h> #include<math.h> int main() {     int i,n,cou=0;     scanf("%d",&n);     for(i=1;i<=n;i++)     {         if(n%i==0)         {             cou=cou+1;         }     }     if(cou==2)     {         printf("Prime");     }     else     {         printf("Not Prime");     } }

--- Prime.c
+++ Prime.c
@@ -1,23 +1,22 @@
 #include<stdio.h>
+#include<math.h>
 int main()
 {
-    int i,n,c=0;
+    int i,n,cou=0;
     scanf("%d",&n);
     for(i=1;i<=n;i++)
     {
         if(n%i==0)
         {
-            c++;
-            
+            cou=cou+1;
         }
     }
-    if(c==2)
+    if(cou==2)
     {
         printf("Prime");
     }
     else
     {
         printf("Not Prime");
-        
     }
 }
