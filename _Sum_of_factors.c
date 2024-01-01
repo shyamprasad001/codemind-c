fb70df74cb495be504f836727506e47e1e336d3b
Shyam Mantri <147488280+shyamprasad001@users.noreply.github.com>
2024-01-01 21:07:57 +0530

    #include<stdio.h> int main() {     int n,i,sum=0;     scanf("%d",&n);     for(i=1;i<n;i++){         if(n%i==0){             sum=sum+i;         }     }     printf("%d",sum);
    }

--- _Sum_of_factors.c
+++ _Sum_of_factors.c
@@ -1,15 +1,14 @@
 #include<stdio.h>
 int main()
 {
-    int i,n,sum=0;
+    int n,i,sum=0;
     scanf("%d",&n);
-    
-    for(i=1;i<=n;i++)
-    {
-        if(n%i==0)
-        {
+    for(i=1;i<n;i++){
+        if(n%i==0){
             sum=sum+i;
         }
     }
-    printf("%d",sum-n);
+    printf("%d",sum);
+        
+    
 }
