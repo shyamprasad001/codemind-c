8155cd1e7498c4e842458513b089f9b493dfbc99
Shyam Mantri <147488280+shyamprasad001@users.noreply.github.com>
2024-01-02 07:10:59 +0530

    #include<stdio.h> int main() {     int i,j,n,k;     scanf("%d",&n);     for(i=n;i>=1;i--)     {         for(j=1;j<=n-i;j++)         {             printf(" ");         }         for(k=1;k<=i;k++)         {             printf("%c ",i+64);         }         printf(" ");     } }

---
+++ Pattern_7.4.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main()
+{
+    int i,j,n,k;
+    scanf("%d",&n);
+    for(i=n;i>=1;i--)
+    {
+        for(j=1;j<=n-i;j++)
+        {
+            printf(" ");
+        }
+        for(k=1;k<=i;k++)
+        {
+            printf("%c ",i+64);
+        }
+        printf("
+");
+    }
+}
