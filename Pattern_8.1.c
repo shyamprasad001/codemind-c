2127fc8a510bc77405ae50f0134943380b38bcb0
Shyam Mantri <147488280+shyamprasad001@users.noreply.github.com>
2024-01-02 07:01:48 +0530

    #include<stdio.h> int main() {     int i,j,n;     scanf("%d",&n);     for(i=1;i<=n;i++)     {         for(j=1;j<=n;j++)         if(i+j==n+1 || i==j)         {             printf("%d ",i);         }         else         {             printf(" ");         }         printf(" ");     } }

---
+++ Pattern_8.1.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main()
+{
+    int i,j,n;
+    scanf("%d",&n);
+    for(i=1;i<=n;i++)
+    {
+        for(j=1;j<=n;j++)
+        if(i+j==n+1 || i==j)
+        {
+            printf("%d ",i);
+        }
+        else
+        {
+            printf(" ");
+        }
+        printf("
+");
+    }
+}
